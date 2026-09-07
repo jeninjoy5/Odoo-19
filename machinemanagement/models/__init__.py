cc100cab81e3c3ceb42347ae7579f05f97b953c9
arjun11pp <arjunraghunath111@gmail.com>
2026-09-07 17:42:44 +0530

    [Update]

--- machinemanagement/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import machine_management
-from . import machine_type
-from . import machine_transfer
-from . import machine_order_line
-from . import machine_tag
-from . import res_partner
-from . import machine_service
--- machinemanagement/models/machine_tag.py
+++
@@ -1,9 +0,0 @@
-from odoo import models,fields
-
-
-class MachineType(models.Model):
-    _name = "machine.tag"
-
-    # FIELDS
-
-    name = fields.Char(string="Name")
